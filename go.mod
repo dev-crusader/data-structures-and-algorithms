31b02dea0b640f14f47d639e74948c65a31c833a
dev-crusader404 <57078175+dev-crusader404@users.noreply.github.com>
2024-10-01 12:07:33 -0500

    sort queue implementations

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dev-crusader404/data-structures-and-algorithms
+
+go 1.22.7
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// ds "github.com/dev-crusader404/data-structures-and-algorithms/datastructure"
+	// bst "github.com/dev-crusader404/data-structures-and-algorithms/datastructure/binarytree"
+	// tp "github.com/dev-crusader404/data-structures-and-algorithms/datastructure/test-proj"
+	// gh "github.com/dev-crusader404/data-structures-and-algorithms/datastructure/graph"
+	// gen "github.com/dev-crusader404/data-structures-and-algorithms/datastructure/generics"
+	// it "github.com/dev-crusader404/data-structures-and-algorithms/datastructure/iterator"
+)
 
 func main() {
 	fmt.Println("Data Structures and Algorithms")
+
+	// srt.SortWithMerge()
+	// ds.BST()
+	// ds.InitHashMap()
+	// ds.CallHeap()
+	// srt.QuickSortApproach()
+	// srt.Radix()
+	// tp.DuplicateRemove()
+	// ds.CallHeap()
+	// bst.BST()
+	// gh.InitGraph()
+	// lc.Run()
+	// gen.RunLinkedList()
+	// gen.GenerateList()
+	// it.RunIterator()
 }

e841bc51a60a0a48a209380082f8fc935ab48323
dev-crusader404 <57078175+dev-crusader404@users.noreply.github.com>
2024-10-02 20:53:31 -0500

    merge sort concurrently

--- datastructure/sort/mergeSort.go
+++ datastructure/sort/mergeSort.go
@@ -1,13 +1,36 @@
 package sort
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"golang.org/x/exp/rand"
+)
 
 func SortWithMerge() {
-	inputArr := []int{50, -8, -96, -63, -73, 95, -69, 16, -38, 53, 72, -71, -92, 25, 59}
-	fmt.Printf("Before:\t%v\n", inputArr)
+	// inputArr := []int{50, -8, -96, -63, -73, 95, -69, 16, -38, 53, 72, -71, -92, 25, 59}
+	inputArr := generateRandomArray(-100, 100, 200)
+	arr := make([]int, len(inputArr))
+	copy(arr, inputArr)
+	// fmt.Printf("Before:\t%v\n", inputArr)
+	t1 := time.Now()
 	mergeSort(inputArr, 0, len(inputArr)-1)
-	fmt.Printf("After:\t%v\n", inputArr)
+	// fmt.Printf("After:\t%v\n", inputArr)
+	fmt.Printf("Took %d nanoseconds\n", time.Since(t1).Nanoseconds())
+
+	// Merge sort implementation concurrently splitting and sorting
+	t2 := time.Now()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go mergeSortConcurrent(arr, 0, len(arr)-1, &wg)
+	wg.Wait()
+	// fmt.Printf("After:\t%v\n", arr)
+	fmt.Printf("Took %d nanoseconds\n", time.Since(t2).Nanoseconds())
+	fmt.Println("Done")
 }
+
 func mergeSort(arr []int, start, end int) {
 	if end <= start {
 		return
@@ -51,4 +74,34 @@ func mergeAllStep(inpArr []int, start, mid, end int) {
 		inpArr[k] = v
 		k++
 	}
+}
+
+func generateRandomArray(min, max, total int) []int {
+	rand.Seed(uint64(time.Now().UnixNano()))
+	var num []int
+
+	for i := 0; i < total; i++ {
+		rn := rand.Intn(max-min) + min
+		num = append(num, rn)
+	}
+	return num
+}
+
+func mergeSortConcurrent(arr []int, left, right int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if right <= left {
+		return
+	}
+
+	mid := (left + right) / 2
+
+	var leftWg, rightWg sync.WaitGroup
+
+	leftWg.Add(1)
+	rightWg.Add(1)
+	go mergeSortConcurrent(arr, left, mid, &leftWg)
+	go mergeSortConcurrent(arr, mid+1, right, &rightWg)
+	leftWg.Wait()
+	rightWg.Wait()
+	mergeAllStep(arr, left, mid, right)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dev-crusader404/data-structures-and-algorithms
 
 go 1.22.7
+
+require golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
